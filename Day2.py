9a48275219102b1283315755fe0935f94cdbfda6
Arif Meman <arifmeman786@gmail.com>
2025-03-04 03:33:03 +0000

    Day2

--- Day2.py
+++ Day2.py
@@ -2,3 +2,14 @@ string = "Hello"
 print(string[4])
 print(string[-1])
 print(string[-2])
+print(len("1234"))
+print(type("123"))
+print(type(123))
+
+################################# Type casting / type conversion ########################
+int("123")
+str(123)
+
+#####################
+
+print("Number of letters in your name: "+str(len(input("Enter your name"))))
